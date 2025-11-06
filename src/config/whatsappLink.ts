7d8d948ef815029eda105f091f3d05e7b1ebc5a9
hoverbe-code <alfredok@me.com>
2025-11-06 10:03:40 -0300

    Ugprade

--- src/config/whatsappLink.ts
+++ src/config/whatsappLink.ts
@@ -1,2 +1,2 @@
 export const whatsappLink =
-  "https://wa.me/5511992579195?text=Oi%20Silvio%2C%20tudo%20bem%3F%20Eu%20estou%20procurando%20hospedagem.%20Voc%C3%AA%20pode%20me%20ajudar%3F";
+  "https://wa.me/5511992579195?text=Ei%20Silvio!%20Gostei%20da%20Click%20Hostel!%20Quero%20reservar%20minha%20vaga!%20Fa%C3%A7a%20uma%20cota%C3%A7%C3%A3o..%20Anote%20meu%20*nome*%2C%20as%20*datas*%20e%20*quantos*%20v%C3%A3o%20comigo%3A";
